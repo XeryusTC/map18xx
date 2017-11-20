53b2078ce2884e72e9e7a9d095b37de06407cff2
XeryusTC <armadillo@onenetbeyond.org>
2017-11-20 22:56:55 +0100

    Accessors for start and end positions of Path

--- src/tile.rs
+++ src/tile.rs
@@ -30,6 +30,33 @@ pub mod colors {
     pub const WHITE:   Color  = Color { value: "#FFFFFF" };
 }
 
+fn edge_to_coordinate(edge: &str) -> na::Point3<f64> {
+    match edge {
+        "N"  => na::Point3::new( 0.0,  0.5, -0.5),
+        "NE" => na::Point3::new( 0.5,  0.0, -0.5),
+        "SE" => na::Point3::new( 0.5, -0.5,  0.0),
+        "S"  => na::Point3::new( 0.0, -0.5,  0.5),
+        "SW" => na::Point3::new(-0.5,  0.0,  0.5),
+        "NW" => na::Point3::new(-0.5,  0.5,  0.0),
+        c => panic!("Invalid edge code {}", c),
+    }
+}
+
+pub struct Tile {
+    number: String,
+    color: colors::Color,
+}
+
+impl Tile {
+    pub fn new(number: String, color: colors::Color) -> Tile {
+        Tile { number, color }
+    }
+
+    pub fn color(&self) -> &str {
+        self.color.value()
+    }
+}
+
 #[derive(Deserialize, Debug)]
 pub struct TileDefinition {
     path: Option<Vec<Path>>,
@@ -54,6 +81,28 @@ pub struct Path {
     end_pos: Option<Box<[f64]>>,
     stops: Option<u32>,
     revenue: Option<u32>,
+}
+
+impl Path {
+    pub fn start(&self) -> na::Point3<f64> {
+        match &self.start_pos {
+            &Some(ref pos) => na::Point3::new(pos[0], pos[1], pos[2]),
+            &None => match &self.start {
+                &Some(ref s) => edge_to_coordinate(s.as_ref()),
+                &None => panic!("No start position found"),
+            }
+        }
+    }
+
+    pub fn end(&self) -> na::Point3<f64> {
+        match &self.end_pos {
+            &Some(ref pos) => na::Point3::new(pos[0], pos[1], pos[2]),
+            &None => match &self.end {
+                &Some(ref s) => edge_to_coordinate(s.as_ref()),
+                _ => panic!("No end position found"),
+            }
+        }
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -94,4 +143,178 @@ pub fn definitions() -> HashMap<String, TileDefinition> {
                            tile);
     }
     definitions
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate toml;
+    use super::{TileDefinition, na};
+
+    #[test]
+    #[should_panic(expected = "No start position found")]
+    fn path_panics_on_no_start_found() {
+        let tile: TileDefinition = toml::from_str("[[path]]").unwrap();
+        tile.paths().unwrap()[0].start();
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid edge code A")]
+    fn path_panics_on_start_invalid_code() {
+        let tile: TileDefinition = toml::from_str(
+            r#"path = [{start = "A"}]"#).unwrap();
+        tile.paths().unwrap()[0].start();
+    }
+
+    #[test]
+    #[should_panic(expected = "No end position found")]
+    fn path_panics_on_no_end_found() {
+        let tile: TileDefinition = toml::from_str("[[path]]").unwrap();
+        tile.paths().unwrap()[0].end();
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid edge code B")]
+    fn path_panics_on_end_invalid_code() {
+        let tile: TileDefinition = toml::from_str(
+            r#"path = [{end = "B"}]"#).unwrap();
+        tile.paths().unwrap()[0].end();
+    }
+
+    #[test]
+    fn path_start_returns_start_pos_when_both_given() {
+        let tile: TileDefinition = toml::from_str(r#"
+            [[path]]
+            start_pos = [-0.1, 0.1, 0.0]
+            start = "N"
+            "#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].start(),
+                   na::Point3::new(-0.1_f64, 0.1, 0.0));
+    }
+
+    #[test]
+    fn path_converts_start_pos() {
+        let tile: TileDefinition = toml::from_str(r#"
+        [[path]]
+        start_pos = [0.1, 0.3, 0.7]
+        "#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].start(),
+                   na::Point3::new(0.1_f64, 0.3, 0.7));
+    }
+
+    #[test]
+    fn path_converts_start_n() {
+        let tile: TileDefinition = toml::from_str(
+            r#"path = [{start = "N"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].start(),
+                   na::Point3::new(0.0, 0.5, -0.5));
+    }
+
+    #[test]
+    fn path_converts_start_ne() {
+        let tile: TileDefinition = toml::from_str(
+            r#"path = [{start = "NE"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].start(),
+                   na::Point3::new(0.5, 0.0, -0.5));
+    }
+
+    #[test]
+    fn path_converts_start_nw() {
+        let tile: TileDefinition = toml::from_str(
+            r#"path = [{start = "NW"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].start(),
+                   na::Point3::new(-0.5, 0.5, 0.0));
+    }
+
+    #[test]
+    fn path_converts_start_s() {
+        let tile: TileDefinition = toml::from_str(
+            r#"path = [{start = "S"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].start(),
+                   na::Point3::new(0.0, -0.5, 0.5));
+    }
+
+    #[test]
+    fn path_converts_start_se() {
+        let tile: TileDefinition = toml::from_str(
+            r#"path = [{start = "SE"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].start(),
+                   na::Point3::new(0.5, -0.5, 0.0));
+    }
+
+    #[test]
+    fn path_converts_start_sw() {
+        let tile: TileDefinition = toml::from_str(
+            r#"path = [{start = "SW"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].start(),
+                   na::Point3::new(-0.5, 0.0, 0.5));
+    }
+
+    #[test]
+    fn path_end_returns_end_pos_when_both_given() {
+        let tile: TileDefinition = toml::from_str(r#"
+            [[path]]
+            end_pos = [0.5, 0.5, 0.5]
+            end = "N"
+            "#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].end(),
+                   na::Point3::new(0.5_f64, 0.5, 0.5));
+    }
+
+    #[test]
+    fn path_converts_end_pos() {
+        let tile: TileDefinition = toml::from_str(r#"
+            [[path]]
+            end_pos = [0.2, 0.4, 0.6]
+            "#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].end(),
+                   na::Point3::new(0.2_f64, 0.4, 0.6));
+    }
+
+    #[test]
+    fn path_converts_end_n() {
+        let tile:TileDefinition = toml::from_str(
+            r#"path = [{end = "N"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].end(),
+                   na::Point3::new(0.0, 0.5, -0.5));
+    }
+
+    #[test]
+    fn path_converts_end_ne() {
+        let tile:TileDefinition = toml::from_str(
+            r#"path = [{end = "NE"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].end(),
+                   na::Point3::new(0.5, 0.0, -0.5));
+    }
+
+    #[test]
+    fn path_converts_end_nw() {
+        let tile:TileDefinition = toml::from_str(
+            r#"path = [{end = "NW"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].end(),
+                   na::Point3::new(-0.5, 0.5, 0.0));
+    }
+
+    #[test]
+    fn path_converts_end_s() {
+        let tile:TileDefinition = toml::from_str(
+            r#"path = [{end = "S"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].end(),
+                   na::Point3::new(0.0, -0.5, 0.5));
+    }
+
+    #[test]
+    fn path_converts_end_se() {
+        let tile:TileDefinition = toml::from_str(
+            r#"path = [{end = "SE"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].end(),
+                   na::Point3::new(0.5, -0.5, 0.0));
+    }
+
+    #[test]
+    fn path_converts_end_sw() {
+        let tile:TileDefinition = toml::from_str(
+            r#"path = [{end = "SW"}]"#).unwrap();
+        assert_eq!(tile.paths().unwrap()[0].end(),
+                   na::Point3::new(-0.5, 0.0, 0.5));
+    }
 }
