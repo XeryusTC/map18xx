fa1ceca0442570995f2dc8cfe04caa169c6ad80a
XeryusTC <armadillo@onenetbeyond.org>
2017-11-23 03:16:08 +0100

    Scale size of document properly

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,7 +9,8 @@ fn main() {
     let info = map::MapInfo::default();
     let document = Document::new()
         .set("width", 11.5 * info.scale)
-        .set("height", 0.4 * info.scale * definitions.len() as f64)
+        .set("height",
+             2.0 * info.scale * (definitions.len() as f64 / 5.0).ceil())
         .add(draw::draw_tile_definitions(&definitions));
 
     svg::save("definitions.svg", &document).unwrap();
