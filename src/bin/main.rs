685f33ad8694d1caa1bc5114adfd8f04ab18f03d
XeryusTC <armadillo@onenetbeyond.org>
2017-11-26 22:51:12 +0100

    Add argument parser, make definitions and game mode skeletons

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,5 @@
 extern crate map18xx;
-extern crate svg;
-
-use map18xx::{draw, map, tile};
-use svg::Document;
 
 fn main() {
-    let definitions = tile::definitions();
-    let info = map::MapInfo::default();
-    let document = Document::new()
-        .set("width", 11.5 * info.scale)
-        .set("height",
-             2.0 * info.scale * (definitions.len() as f64 / 5.0).ceil())
-        .add(draw::draw_tile_definitions(&definitions));
-
-    svg::save("definitions.svg", &document).unwrap();
+    map18xx::run()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+extern crate argparse;
 #[macro_use]
 extern crate serde_derive;
+extern crate svg;
+
+use argparse::ArgumentParser;
 
 pub mod draw;
 pub mod tile;
@@ -11,4 +15,58 @@ pub enum Orientation {
     Horizontal,
     /// Hexes should have apoint at the top
     Vertical,
+}
+
+/// Place to store command line options
+struct Options {
+    game: Option<String>,
+}
+
+impl Options {
+    pub fn new() -> Options {
+        Options {
+            game: None,
+        }
+    }
+}
+
+pub fn run() {
+    let mut options = Options::new();
+    { // Limit scope of ArgumentParser borrow
+        let mut parser = ArgumentParser::new();
+        parser.set_description(
+            "18xx tile and map designer. Will generate definitions.svg when no
+            mode arguments given. Game mode can be used with --game.");
+        parser.add_option(&["-V", "--version"],
+                          argparse::Print(env!("CARGO_PKG_VERSION")
+                                          .to_string()),
+                          "Show version");
+        parser.refer(&mut options.game)
+            .add_option(&["-g", "--game"],
+                        argparse::StoreOption,
+                        "Generate files for a game map")
+            .metavar("MAP");
+        parser.parse_args_or_exit();
+    }
+
+    if let &Some(ref name) = &options.game {
+        game_mode(name, &options);
+    } else {
+        definitions();
+    }
+}
+
+fn definitions() {
+    let definitions = tile::definitions();
+    let info = map::MapInfo::default();
+    let document = svg::Document::new()
+        .set("width", 11.5 * info.scale)
+        .set("height",
+             2.0 * info.scale * (definitions.len() as f64 / 5.0).ceil())
+        .add(draw::draw_tile_definitions(&definitions));
+    svg::save("definitions.svg", &document).unwrap();
+}
+
+fn game_mode(name: &String, options: &Options) {
+    println!("Processing map '{}'", name);
 }
