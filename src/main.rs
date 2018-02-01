0465c081c83b5487398455032d8a3728c4a4e438
XeryusTC <armadillo@onenetbeyond.org>
2018-02-01 02:33:56 +0100

    Use clap instead of argparse for CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,43 @@
+#[macro_use]
+extern crate clap;
 extern crate map18xx;
 
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    map18xx::run()
+    let matches = App::new("map18xx")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("18xx tile and map designer")
+        .arg(Arg::with_name("verbose")
+             .help("Print debug information")
+             .short("v")
+             .long("verbose"))
+        .arg(Arg::with_name("pretty_coordinates")
+             .help("Show coordinates on each row/column")
+             .long("pretty_coordinates")
+             .aliases(&["pc"]))
+        .subcommand(SubCommand::with_name("asset")
+                    .about("Generate assets to PnP game")
+                    .aliases(&["a", "assets"])
+                    .arg(Arg::with_name("game")
+                         .help("Game for which to generate assets")
+                         .required(true)
+                         .index(1)))
+        .get_matches();
+
+    let mut options = map18xx::Options::new();
+    options.verbose = matches.is_present("verbose");
+    options.pretty_coordinates = matches.is_present("pretty_coordinates");
+
+    // Determine subcommand
+    match matches.subcommand() {
+        ("asset", Some(ref matches)) => {
+            let mut asset_options = map18xx::AssetOptions::new();
+            asset_options.name = matches.value_of("game").unwrap().to_string();
+            map18xx::asset_mode(&options, &asset_options);
+        }
+        ("", _) => map18xx::definitions(&options),
+        (name, _) => eprintln!("Unkown subcommand {}.", name),
+    }
 }
